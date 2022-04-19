7d88c41b94a346d0a199045339a9ab2c8a2d545c
Maria Muhina <mvvoler@gmail.com>
2022-04-19 13:18:50 +0300

    5.1_2

--- Задание 2.2/secondGY.py
+++ Задание 2.2/secondGY.py
@@ -4,8 +4,6 @@ from scipy.sparse import linalg
 from matplotlib import pyplot as plt
 import math
 
-task_number = 1
-
 
 def p_analit_1(x):
     global rw
@@ -15,41 +13,6 @@ def p_analit_1(x):
 def u_analit_1(x):
     global rw
     return 1 / math.log(rw) / x
-
-
-def p_analit_2(x):
-    global rw
-    ksi = 1.0
-    mu = 0.1
-    if x <= 0.5:
-        return mu / (mu * math.log(0.5 / rw) - ksi * math.log(0.5)) * math.log(x / rw)
-    else:
-        return ksi / (mu * math.log(0.5 / rw) - ksi * math.log(0.5)) * math.log(x) + 1
-
-
-def u_analit_2(x):
-    global rw
-    ksi = 1.0
-    mu = 0.1
-    return ksi * mu / (ksi * math.log(0.5) - mu * math.log(0.5 / rw)) / x
-
-
-def p_analit_3(x):
-    # K = 0.0440824
-    global rw
-    ksi = 1.0
-    mu = 0.0440824
-    if x <= 0.75:
-        return mu / (mu * math.log(0.75 / rw) - ksi * math.log(0.75)) * math.log(x / rw)
-    else:
-        return ksi / (mu * math.log(0.75 / rw) - ksi * math.log(0.75)) * math.log(x) + 1
-
-
-def u_analit_3(x):
-    global rw
-    ksi = 1.0
-    mu = 0.0440824
-    return ksi * mu / (ksi * math.log(0.75) - mu * math.log(0.75 / rw)) / x
 
 
 def p_ind(i: int) -> int:
@@ -85,6 +48,7 @@ def define_type_x(xx):
 
 # Разбиение минимальной зоны
 N_min = 10000
+q = 0.909584236
 
 # Кол-во зон и их границы
 with open('zone_info.txt', 'r') as file:
@@ -128,20 +92,46 @@ for i in range(xs):
         eq_num += 1
 
 # Граничные условия для p
-col_ind.append(p_ind(0))
-row_ind.append(eq_num)
-data.append(1)
+col_ind.extend([p_ind(0), p_ind(1), u_ind(0)])
+row_ind.extend([eq_num, eq_num, eq_num])
+# data.extend([1/h[0], -1/h[0], -q/(2 * math.pi * rw)])
+data.extend([1, -1, -h[0]*rw/(rw+h[0]/2)])
+# data.extend([1, -1, -h[0]*rw/(rw*(1/rw)**1/N_min)])
 eq_num += 1
+# col_ind.extend([p_ind(0), p_ind(1)])
+# row_ind.extend([eq_num, eq_num])
+# data.extend([-(rw+h[0]/2)/h[0], (rw+h[0]/2)/h[0]])
+# rhs[eq_num] = q/(2*math.pi)
+# eq_num += 1
+# col_ind.extend([p_ind(1), p_ind(2), u_ind(0)])
+# row_ind.extend([eq_num, eq_num, eq_num])
+# data.extend([(rw+h[0]/2+rw+h[0]+h[1]/2)/(2*h[1]), -(rw+h[0]/2+rw+h[0]+h[1]/2)/(2*h[1]), rw*(rw+rw+h[0]/2)/(2*(rw+h[0]/2))])
+# rhs[eq_num] = q/(2*math.pi)
+# eq_num += 1
+
+
 col_ind.append(p_ind(xs - 1))
 row_ind.append(eq_num)
 data.append(1)
 rhs[eq_num] = 1
 eq_num += 1
+
 # Граничные условия для u
-col_ind.extend([p_ind(0), p_ind(1), u_ind(0)])
-row_ind.extend([eq_num, eq_num, eq_num])
-data.extend([f[0] / h[0], -f[0] / h[0], -1])
+col_ind.append(u_ind(0))
+row_ind.append(eq_num)
+data.append(rw)
+rhs[eq_num] = -q / (2 * math.pi)
 eq_num += 1
+# col_ind.extend([p_ind(0), p_ind(1), u_ind(0)])
+# row_ind.extend([eq_num, eq_num, eq_num])
+# data.extend([f[0] / h[0], -f[0] / h[0], -1])
+# eq_num += 1
+# col_ind.extend([p_ind(1), p_ind(2), u_ind(0)])
+# row_ind.extend([eq_num, eq_num, eq_num])
+# data.extend([(rw+h[0]/2+rw+h[0]+h[1]/2)/(2*h[1]), -(rw+h[0]/2+rw+h[0]+h[1]/2)/(2*h[1]), rw*(rw+rw+h[0]/2)/(2*(rw+h[0]/2))])
+# rhs[eq_num] = q/(2*math.pi)
+# eq_num += 1
+
 col_ind.extend([p_ind(xs - 2), p_ind(xs - 1), u_ind(xs - 1)])
 row_ind.extend([eq_num, eq_num, eq_num])
 data.extend([f[xs - 2] / h[xs - 2], -f[xs - 2] / h[xs - 2], -1])
@@ -164,20 +154,14 @@ p_u = linalg.spsolve(sA, rhs)
 p = p_u[:xs]
 u = p_u[xs:]
 
-if task_number == 1:
-    p_an = [p_analit_1(x[i]) for i in range(xs)]
-    u_an = [u_analit_1(x[i]) for i in range(xs)]
-elif task_number == 2:
-    p_an = [p_analit_2(x[i]) for i in range(xs)]
-    u_an = [u_analit_2(x[i]) for i in range(xs)]
-elif task_number == 3:
-    p_an = [p_analit_3(x[i]) for i in range(xs)]
-    u_an = [u_analit_3(x[i]) for i in range(xs)]
-else:
-    p_an = np.zeros(xs)
-    u_an = np.zeros(xs)
+
+p_an = [p_analit_1(x[i]) for i in range(xs)]
+u_an = [u_analit_1(x[i]) for i in range(xs)]
+
 # Расчет невязки
 E = math.sqrt(sum([(p[i] - p_an[i]) ** 2 for i in range(xs)]) / xs)
+print(p[0])
+print(u[0])
 
 plt.figure(1)
 plt.plot(x, p_an, 'b', x, p, 'r--')
@@ -192,11 +176,9 @@ plt.grid(which='major', linewidth=1)
 plt.grid(which='minor', linestyle=':')
 plt.grid(which='minor', linestyle=':')
 plt.ylim([-0.1, 1.1])
-name_p_file = f'Функция давления (задание {task_number}) N={xs}.png'
-if task_number in [1, 2, 3]:
-    plt.savefig(name_p_file, dpi=300)
-else:
-    plt.show()
+name_p_file = f'Функция давления (задание 2.1.1) N={xs}.png'
+plt.savefig(name_p_file, dpi=300)
+
 
 plt.figure(2)
 plt.plot(x, u_an, 'b', x, u, 'r--')
@@ -210,32 +192,17 @@ plt.minorticks_on()
 plt.grid(which='major', linewidth=1)
 plt.grid(which='minor', linestyle=':')
 plt.grid(which='minor', linestyle=':')
-name_u_file = f'Функция скорости фильтрации (задание {task_number}) N={xs}.png'
-if task_number in [1, 2, 3]:
-    plt.savefig(name_u_file, dpi=300)
-else:
-    plt.show()
+name_u_file = f'Функция скорости фильтрации (задание 2.1.1) N={xs}.png'
+plt.savefig(name_u_file, dpi=300)
+
 
 R, rw, k0, m, dp, mu = 100, 0.1, 1e-12, 0.2, 1e6, 1e-3
 
-if task_number == 1:
-    # T = mu * m * math.log(R / rw) * (R ** 2 - rw ** 2) / 2 / k0 / (p[xs - 1] - p[0]) / dp
-    # T = sum([h[i] * h[i] * R * mu * m * (R - rw) / k0 / dp / (p[i + 1] - p[i]) for i in range(xs - 1)])
-    T = sum([mu * (R - rw) * m / u[i] / k0 / dp * h[i] * (R - rw) for i in range(xs-1)])
-elif task_number == 2:
-    # T = mu * m * math.log(0.5 * R / rw) * ((0.5 * R) ** 2 - rw ** 2) / 2 / k0 / (p[N[0]] - p[0]) / dp
-    # T += mu * m * math.log(R / (0.5 * R)) * (R ** 2 - (0.5 * R) ** 2) / 2 / k0 / (p[xs - 1] - p[N[0]]) / dp
-    # T = sum([h[i] * h[i] * R * mu * m * (R - rw) / k0 / dp / (p[i + 1] - p[i]) for i in range(xs - 1)])
-    T = sum([mu * (R - rw) * m / u[i] / k0 / dp * h[i] * (R - rw) for i in range(xs-1)])
-elif task_number == 3:
-    # T = mu * m * math.log(0.75 * R / rw) * ((0.75 * R) ** 2 - rw ** 2) / 2 / k0 / (p[N[0]] - p[0]) / dp
-    # T += mu * m * math.log(R / (0.75 * R)) * (R ** 2 - (0.75 * R) ** 2) / 2 / k0 / (p[xs - 1] - p[N[0]]) / dp
-    # T = sum([h[i] * h[i] * R * mu * m * (R - rw) / k0 / dp / (p[i + 1] - p[i]) for i in range(xs - 1)])
-    T = sum([mu * (R - rw) * m / u[i] / k0 / dp * h[i] * (R - rw) for i in range(xs-1)])
 
-if task_number in [1, 2, 3]:
-    with open(f'results (задание {task_number}).txt', 'w', encoding='utf-8') as file:
-        file.write(f'Невязка при расчете давления: {E}\n')
-        file.write(f'Время прохождения частицы между галереями: {T}')
+# T = mu * m * math.log(R / rw) * (R ** 2 - rw ** 2) / 2 / k0 / (p[xs - 1] - p[0]) / dp
+# T = sum([h[i] * h[i] * R * mu * m * (R - rw) / k0 / dp / (p[i + 1] - p[i]) for i in range(xs - 1)])
+T = sum([mu * (R - rw) * m / u[i] / k0 / dp * h[i] * (R - rw) for i in range(xs-1)])
 
-print(u[0]*rw)
+with open(f'results (задание 2.1.1).txt', 'w', encoding='utf-8') as file:
+    file.write(f'Невязка при расчете давления: {E}\n')
+    file.write(f'Время прохождения частицы между галереями: {T}')
